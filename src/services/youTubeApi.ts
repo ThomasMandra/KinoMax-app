9f374c715616375e5ca1b467e57ebeab5b2b90d1
ThomasMandra <starikov.ru@bk.ru>
2025-05-03 16:45:55 +0700

    cleaned comment youTubeApi

--- src/services/youTubeApi.ts
+++ src/services/youTubeApi.ts
@@ -1,49 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const youtubeApiKey = import.meta.env.VITE_YOUTUBE_KEY;
-
-const excludeGenres = [
-  "",
-  "новости",
-  "для взрослых",
-  "церемония",
-  "реальное ТВ",
-  "ток-шоу",
-];
-
-// Определение интерфейса для параметров запроса к API Kinopoisk
-interface KinopoiskApiQuery {
-  type: string | undefined;
-  page: number;
-}
-
-// Определение интерфейса для структуры ответа API
-
-interface MoviesListItems {
-  nameRu: string;
-  kinopoiskId: number;
-  posterUrlPreview: string;
-  [key: string]: string | number;
-}
-
-interface KinopoiskApiResponse {
-  items: MoviesListItems[];
-  totalPages: number;
-}
-
-// Определение интерфейса для структуры ответа API Сиквелы Приквелы
-
-interface SequelsAndPrequels {
-  filmId: number;
-  nameRu?: string;
-  nameEn?: string;
-  nameOrigina?: string;
-  posterUrl?: string;
-  posterUrlPreview?: string;
-  elationType?: string;
-}
-
-// Define a service using a base URL and expected endpoints
 export const youTubeApi = createApi({
   reducerPath: "youTubeApi",
   baseQuery: fetchBaseQuery({
@@ -65,14 +21,3 @@ export const youTubeApi = createApi({
 });
 
 export const { useGetTrailerQuery } = youTubeApi;
-
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-// export const {
-//   useGetFilmsTopQuery,
-//   useGetFilmsQuery,
-//   useGetGenresAndCountriesQuery,
-//   useGetFilmQuery,
-//   useGetSequelsAndPrequelsQuery,
-//   useGetStaffQuery,
-// } = kinopoiskApi;
